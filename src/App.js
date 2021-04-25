dad707c4c9a9b0355d476f786ea928a54b0b5212
Andre Souza <ajjunior33@gmail.com>
2021-04-24 23:02:12 -0300

    Firulinhas,

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { BiToggleLeft, BiToggleRight } from "react-icons/bi";
+import { DiGithubBadge } from 'react-icons/di'
 import "./App.css";
 function App() {
   const [color, setColor] = useState("");
   const [style, setStyle] = useState({});
-  const [styleColor, setStyleColor] = useState({});
+  const [styleColor, setStyleColor] = useState({ color: "#222" });
 
   const [problem, setProblem] = useState("protanopia");
 
@@ -38,7 +39,11 @@ function App() {
       conversaoRgb[0] = 0;
     }
     calcLuminosidade(conversaoRgb[0], conversaoRgb[1], conversaoRgb[2]);
-    const conversaoHex = toHex(conversaoRgb[0], conversaoRgb[1], conversaoRgb[2]);
+    const conversaoHex = toHex(
+      conversaoRgb[0],
+      conversaoRgb[1],
+      conversaoRgb[2]
+    );
     saveColor(conversaoHex);
     console.log(conversaoRgb);
   }
@@ -59,7 +64,7 @@ function App() {
 
   function calcLuminosidade(r, g, b) {
     var lum;
-    lum = Math.sqrt((r * r * 0.299) + (g * g * 0.587) + (b * b * 0.114)); // Conversao w3c
+    lum = Math.sqrt(r * r * 0.299 + g * g * 0.587 + b * b * 0.114); // Conversao w3c
     console.log(lum);
     if (lum < 155) {
       setStyleColor({ color: "#f1f1f1" });
@@ -85,7 +90,6 @@ function App() {
   };
 
   function saveColor(cor = null) {
-
     console.log(cor);
     if (!cor) {
       setColor(color);
@@ -180,7 +184,55 @@ function App() {
               purpose (injected humour and the like).
             </p>
           </article>
+          <article>
+            <h1>Where does it come from?</h1>
+            <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+              It has roots in a piece of classical Latin literature from 45 BC,
+              making it over 2000 years old. Richard McClintock, a Latin
+              professor at Hampden-Sydney College in Virginia, looked up one of
+              the more obscure Latin words, consectetur, from a Lorem Ipsum
+              passage, and going through the cites of the word in classical
+              literature, discovered the undoubtable source. Lorem Ipsum comes
+              from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
+              Malorum" (The Extremes of Good and Evil) by Cicero, written in 45
+              BC. This book is a treatise on the theory of ethics, very popular
+              during the Renaissance. The first line of Lorem Ipsum, "Lorem
+              ipsum dolor sit amet..", comes from a line in section 1.10.32. The
+              standard chunk of Lorem Ipsum used since the 1500s is reproduced
+              below for those interested. Sections 1.10.32 and 1.10.33 from "de
+              Finibus Bonorum et Malorum" by Cicero are also reproduced in their
+              exact original form, accompanied by English versions from the 1914
+              translation by H. Rackham.
+            </p>
+          </article>
+          <article>
+            <h1>Where can I get some?</h1>
+
+            <p>
+              There are many variations of passages of Lorem Ipsum available,
+              but the majority have suffered alteration in some form, by
+              injected humour, or randomised words which don't look even
+              slightly believable. If you are going to use a passage of Lorem
+              Ipsum, you need to be sure there isn't anything embarrassing
+              hidden in the middle of text. All the Lorem Ipsum generators on
+              the Internet tend to repeat predefined chunks as necessary, making
+              this the first true generator on the Internet. It uses a
+              dictionary of over 200 Latin words, combined with a handful of
+              model sentence structures, to generate Lorem Ipsum which looks
+              reasonable. The generated Lorem Ipsum is therefore always free
+              from repetition, injected humour, or non-characteristic words etc.
+            </p>
+          </article>
         </div>
+
+        <footer>
+          <span>Todos os direitos reservados - 2021</span>
+          <span>André Junior</span>
+          <a href="https://github.com/ajjunior33">
+            <DiGithubBadge size={32} style={styleColor}/>
+          </a>
+        </footer>
       </section>
     </div>
   );
